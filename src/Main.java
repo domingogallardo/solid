2d4221c997b4de6b97c8e9395ab4a2e83c3e3211
Domingo Gallardo <domingo.gallardo@ua.es>
2020-10-05 21:15:22 +0200

    Añadido autor

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Blog blog = new Blog("Un blog");
-        int post1 = blog.addPost(new Post("El primer post"));
-        int post2 = blog.addPost(new Post("El segundo post"));
-        int post3 = blog.addPost(new Post("El tercer post"));
+        int post1 = blog.addPost(new Post("El primer post", "Aitana M."));
+        int post2 = blog.addPost(new Post("El segundo post", "Big Foot"));
+        int post3 = blog.addPost(new Post("El tercer post", "Pepa Pig"));
         blog.deletePost(post2);
         System.out.println(blog.print());
     }
--- src/Post.java
+++ src/Post.java
@@ -4,10 +4,12 @@ public class Post {
     private Integer id;
     private String text;
     private Date date;
+    private String author;
 
-    public Post(String text) {
+    public Post(String text, String author) {
         this.text = text;
         this.date = new Date();
+        this.author = author;
     }
 
     public void setId(Integer id) {
@@ -19,6 +21,6 @@ public class Post {
     }
 
     public String print() {
-        return text + " (" + date + ")";
+        return author + " - " + text + " (" + date + ")";
     }
 }
